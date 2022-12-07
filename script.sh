86f3baf00c4357a6d7cc914692084c95169bf80f
chillaxt <nttjoe2020@gmail.com>
2022-12-06 20:02:19 -0800

    Second commit

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sudo apt update # update
 sudo apt upgrade # upgrade
-sudo apt install python3-pip
-sudo pip3 install virtualenv 
-sudo snap install --classic code
+sudo apt autoremove # remove the packages are no longer needed
+sudo apt install python3-pip # install pip
+sudo pip3 install virtualenv # install virtual environment from pip3
+sudo snap install --classic code #install VS code
 
