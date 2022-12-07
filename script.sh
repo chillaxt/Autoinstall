7a60e001cc01efde50aa371770c43a7db4746982
chillaxt <nttjoe2020@gmail.com>
2022-12-06 20:05:58 -0800

    3rd

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update # update
 sudo apt upgrade # upgrade
-sudo apt autoremove # remove the packages are no longer needed
+sudo apt autoremove -y # remove the packages are no longer needed
 sudo apt install python3-pip # install pip
 sudo pip3 install virtualenv # install virtual environment from pip3
 sudo snap install --classic code #install VS code
